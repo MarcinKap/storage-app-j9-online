c0376f062ebceef683ace0d87f1325778243d31b
Marcin Kapcinski <marcin8768@gmail.com>
2019-06-01 11:16:36 +0200

    dodanie nazwy aplikacji po wykonaniu artefaktu w pom.xml

---
+++ src/main/java/akademia/StorageAppJ9OnlineApplication.java
@@ -0,0 +1,13 @@
+package akademia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageAppJ9OnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StorageAppJ9OnlineApplication.class, args);
+    }
+
+}
